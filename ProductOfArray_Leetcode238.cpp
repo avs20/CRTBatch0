a07f3b1cde4ba7b8a753ae2c08dc122409da8267
Ashutosh Singh <dh_ashu@hotmail.com>
2021-01-26 11:28:15 +0530

    Create ProductOfArray_Leetcode238.cpp

---
+++ ProductOfArray_Leetcode238.cpp
@@ -0,0 +1,34 @@
+//Leet code  238 : Product of array except self 
+// Original solution 
+// https://leetcode.com/problems/product-of-array-except-self/
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+        //create a vector with all values intialized to 1 
+        vector<int> products(nums.size(), 1);
+        
+        vector<int> leftProducts(nums.size(), 1);
+        vector<int> rightProducts(nums.size(), 1);
+        
+        int leftRunningProduct  = 1;
+        for(int i = 0; i < nums.size() ; i++ ){
+            leftProducts[i] = leftRunningProduct;
+            leftRunningProduct = leftRunningProduct * nums[i];
+        }
+        
+        int rightRunningProduct  = 1;
+        for(int i = nums.size() - 1; i >=0  ; i-- ){
+            rightProducts[i] = rightRunningProduct;
+            rightRunningProduct = rightRunningProduct * nums[i];
+        }
+        
+        for(int i = 0 ; i < nums.size() ;i++){
+            products[i] = leftProducts[i] * rightProducts[i];
+        }
+        
+        
+        return products;
+    }
+};
